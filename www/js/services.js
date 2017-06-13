4f0a524a28a9a1eb4e1266e1377a5a24b23dda4a
BME319 <zjubme319@163.com>
2017-06-13 22:21:31 +0800

    添加token验证
    Signed-off-by: BME319 <zjubme319@163.com>

--- www/js/services.js
+++ www/js/services.js
@@ -857,9 +857,9 @@ angular.module('kidney.services', ['ionic','ngResource'])
 
     var User = function(){
         return $resource(CONFIG.baseUrl + ':path/:route',{path:'user'},{
-            register:{method:'POST', params:{route: 'register',phoneNo:'@phoneNo',password:'@password',role:'@role'}, timeout: 100000},
-            changePassword:{method:'POST', params:{route: 'reset',phoneNo:'@phoneNo',password:'@password'}, timeout: 100000},
-            logIn:{method:'POST', params:{route: 'login'}, timeout: 100000},
+            register:{method:'POST', params:{route: 'register', skipAuthorization: true, phoneNo:'@phoneNo',password:'@password',role:'@role'}, timeout: 100000},
+            changePassword:{method:'POST', params:{route: 'reset', skipAuthorization: true, phoneNo:'@phoneNo',password:'@password'}, timeout: 100000},
+            logIn:{method:'POST', params:{route: 'login', skipAuthorization: true}, timeout: 100000},
             logOut:{method:'POST', params:{route: 'logout',userId:'@userId'}, timeout: 100000},
             getUserId:{method:'GET', params:{route: 'getUserID',phoneNo:'@phoneNo'}, timeout: 100000},
             sendSMS:{method:'POST', params:{route: 'sendSMS',mobile:'@mobile',smsType:'@smsType'}, timeout: 100000},//第一次验证码发送成功返回结果为”User doesn't exist“，如果再次发送才返回”验证码成功发送“
@@ -944,8 +944,8 @@ angular.module('kidney.services', ['ionic','ngResource'])
 
     var wechat = function(){
         return $resource(CONFIG.baseUrl + ':path/:route',{path:'wechat'},{
-            settingConfig:{method:'GET', params:{route: 'settingConfig'}, timeout: 100000},
-            getUserInfo:{method:'GET', params:{route: 'getUserInfo'}, timeout: 100000},
+            settingConfig:{method:'GET', params:{route: 'settingConfig', skipAuthorization: true}, timeout: 100000},
+            getUserInfo:{method:'GET', params:{route: 'getUserInfo', skipAuthorization: true}, timeout: 100000},
             download:{method:'GET', params:{route: 'download'}, timeout: 100000},
             messageTemplate:{method:'POST', params:{route: 'messageTemplate'}, timeout: 100000},
             createTDCticket:{method:'POST', params:{route: 'createTDCticket'}, timeout: 100000}
