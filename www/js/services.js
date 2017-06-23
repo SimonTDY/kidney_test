db695f342c2b8157c52d73a4d03ca46af944dd7a
BME319 <zjubme319@163.com>
2017-06-23 19:48:45 +0800

    timeout修改
    Signed-off-by: BME319 <zjubme319@163.com>

--- www/js/services.js
+++ www/js/services.js
@@ -861,7 +861,7 @@ angular.module('kidney.services', ['ionic','ngResource'])
         return $resource(CONFIG.baseUrl + ':path/:route',{path:'user'},{
             register:{method:'POST', skipAuthorization: true, params:{route: 'register', phoneNo:'@phoneNo',password:'@password',role:'@role'}, timeout: 100000},
             changePassword:{method:'POST', skipAuthorization: true, params:{route: 'reset', phoneNo:'@phoneNo',password:'@password'}, timeout: 100000},
-            logIn:{method:'POST', skipAuthorization: true, params:{route: 'login'}, timeout: 100000},
+            logIn:{method:'POST', skipAuthorization: true, params:{route: 'login'}, timeout: 10000},
             logOut:{method:'POST', params:{route: 'logout',userId:'@userId'}, timeout: 100000},
             getUserId:{method:'GET', skipAuthorization: true,params:{route: 'userID',username:'@username'}, timeout: 100000},
             sendSMS:{method:'POST', skipAuthorization: true,params:{route: 'sms',mobile:'@mobile',smsType:'@smsType'}, timeout: 100000},//第一次验证码发送成功返回结果为”User doesn't exist“，如果再次发送才返回”验证码成功发送“
@@ -947,7 +947,7 @@ angular.module('kidney.services', ['ionic','ngResource'])
     var wechat = function(){
         return $resource(CONFIG.baseUrl + ':path/:route',{path:'wechat'},{
             settingConfig:{method:'GET', skipAuthorization: true, params:{route: 'settingConfig'}, timeout: 100000},
-            getUserInfo:{method:'GET', skipAuthorization: true, params:{route: 'getUserInfo'}, timeout: 100000},
+            getUserInfo:{method:'GET', skipAuthorization: true, params:{route: 'getUserInfo'}, timeout: 10000},
             download:{method:'GET', params:{route: 'download'}, timeout: 100000},
             messageTemplate:{method:'POST', params:{route: 'messageTemplate'}, timeout: 100000},
             createTDCticket:{method:'POST', params:{route: 'createTDCticket'}, timeout: 100000}
