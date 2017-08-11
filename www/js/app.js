74b9def42dcc9da6083af5111ec17a3cd4b5ed90
BME319 <zjubme319@163.com>
2017-08-11 19:29:07 +0800

    隐藏欢迎图片
    Signed-off-by: BME319 <zjubme319@163.com>

--- www/js/app.js
+++ www/js/app.js
@@ -967,6 +967,8 @@ angular.module('kidney', [
 //   $httpProvider.interceptors.push('jwtInterceptor')
 // }])
 .controller('tabCtrl', ['$state', '$scope', function ($state, $scope) {
+  documnent.getElementById('splashimg').style.display = 'none'
+
   $scope.goHome = function () {
     setTimeout(function () {
       $state.go('tab.home', {})
